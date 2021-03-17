349de932cd35e05979a2ecbc15b72e485ddb9bf5
petrarch <811460602@qq.com>
2021-03-18 02:19:04 +0800

    convert to es

--- src/markdown.js
+++ src/markdown.js
@@ -1,7 +1,6 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module) }
-
-// Include all our dependencies and return the resulting library.
-
-define(['./parser', './markdown_helpers', './render_tree', './dialects/gruber', './dialects/maruku'], function(Markdown) {
-  return Markdown;
-});
+import Markdown from "./parser";
+import "./markdown_helpers";
+import "./render_tree";
+import "./dialects/gruber";
+import "./dialects/maruku";
+export default Markdown;
